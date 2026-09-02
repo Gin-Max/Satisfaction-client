0a035fb659409986491b2c2ba43443a17fc06aaf
Gin-Max <maxence.elkhader@gmail.com>
2026-09-02 17:30:29 +0200

    test: avoid Airflow metadata database in DAG tests

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -6,6 +6,7 @@ from airflow.utils.dag_cycle_tester import check_cycle
 
 
 DAGS_FOLDER = Path(__file__).resolve().parents[1] / "airflow" / "dags"
+DAG_ID = "scraping_reviews_weekly"
 
 
 @pytest.fixture(scope="module")
@@ -16,17 +17,21 @@ def dagbag():
     )
 
 
+@pytest.fixture(scope="module")
+def scraping_dag(dagbag):
+    assert DAG_ID in dagbag.dags, f"DAG introuvable : {DAG_ID}"
+    return dagbag.dags[DAG_ID]
+
+
 def test_dags_load_without_errors(dagbag):
     assert dagbag.import_errors == {}, dagbag.import_errors
 
 
 def test_scraping_dag_is_present(dagbag):
-    assert "scraping_reviews_weekly" in dagbag.dags
+    assert DAG_ID in dagbag.dags
 
 
-def test_scraping_dag_has_expected_tasks(dagbag):
-    dag = dagbag.get_dag("scraping_reviews_weekly")
-
+def test_scraping_dag_has_expected_tasks(scraping_dag):
     expected_tasks = {
         "backfill_existing_reviews",
         "scrape_trustpilot",
@@ -37,7 +42,7 @@ def test_scraping_dag_has_expected_tasks(dagbag):
         "load_google",
     }
 
-    actual_tasks = {task.task_id for task in dag.tasks}
+    actual_tasks = {task.task_id for task in scraping_dag.tasks}
 
     assert expected_tasks.issubset(actual_tasks)
 
@@ -47,10 +52,8 @@ def test_dags_have_no_cycles(dagbag):
         check_cycle(dag)
 
 
-def test_scraping_tasks_have_failure_callback(dagbag):
-    dag = dagbag.get_dag("scraping_reviews_weekly")
-
-    for task in dag.tasks:
+def test_scraping_tasks_have_failure_callback(scraping_dag):
+    for task in scraping_dag.tasks:
         assert task.on_failure_callback is not None, (
             f"La tâche {task.task_id} n'a pas de callback d'échec"
         )
