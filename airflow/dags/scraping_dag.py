9d12e57ad12079fe99b60ce6d5cbfb7bbc77e17e
Eleonore <mar24bda@gmail.com>
2026-09-07 13:05:59 +0200

    test CI imports

--- airflow/dags/scraping_dag.py
+++ airflow/dags/scraping_dag.py
@@ -29,10 +29,10 @@ def pipeline():
     def init_ES():
         """Crée l'index Elasticsearch si nécessaire et charge l'historique
         Trustpilot/Google si aucune donnée n'est encore présente pour ces sources."""
+        from scraping.extract_google import is_google_empty, load_historical_google_reviews
+        from scraping.extract_trustpilot import is_trustpilot_empty, load_historical_reviews
         from scraping.load import INDEX_NAME, create_index_if_not_exists, get_es_client, load_to_elasticsearch
         from scraping.transform import transform
-        from scraping.extract_trustpilot import is_trustpilot_empty, load_historical_reviews
-        from scraping.extract_google import is_google_empty, load_historical_google_reviews
 
         client = get_es_client()
         create_index_if_not_exists(client, INDEX_NAME)
